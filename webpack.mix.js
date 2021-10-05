04efc78de444d155ccf329c34c49946e9a933349
Sergei Toroptsev <sergey.toroptsev@gmail.com>
2021-10-05 19:37:08 +0300

    now compiles separated files

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,18 +2,35 @@ let mix = require('laravel-mix');
 
 require('mix-tailwindcss');
 
+let fs  = require('fs');
+
+let getFiles = function (dir) {
+    return fs.readdirSync(dir).filter(file => {
+        return fs.statSync(`${dir}/${file}`).isFile();
+    });
+};
+
+getFiles('./themes/' + process.env.ACTIVE_THEME + '/assets/src/js/').forEach(function (JSpath) {
+    mix
+        .setPublicPath('./themes/' + process.env.ACTIVE_THEME + '/assets/')
+        .setResourceRoot('/themes/' + process.env.ACTIVE_THEME + '/assets/')
+        .js('./themes/' + process.env.ACTIVE_THEME + '/assets/src/js/' + JSpath, 'public/js')
+        .sourceMaps(false)
+});
+
+getFiles('./themes/' + process.env.ACTIVE_THEME + '/assets/src/css/').forEach(function (CSSpath) {
+    mix
+        .setPublicPath('./themes/' + process.env.ACTIVE_THEME + '/assets/')
+        .setResourceRoot('/themes/' + process.env.ACTIVE_THEME + '/assets/')
+        .postCss('./themes/' + process.env.ACTIVE_THEME + '/assets/src/css/' + CSSpath, 'public/css')
+        .tailwind()
+        .options({
+            processCssUrls: false
+        })
+        .sourceMaps(false)
+});
+
 mix
-    .setPublicPath('./themes/' + process.env.ACTIVE_THEME + '/assets/')
-    .setResourceRoot('/themes/' + process.env.ACTIVE_THEME + '/assets/')
-    .js('./themes/' + process.env.ACTIVE_THEME + '/assets/src/js/alpine.js', 'public/js')
-    .js('./themes/' + process.env.ACTIVE_THEME + '/assets/src/js/lazysizes.js', 'public/js')
-    .js('./themes/' + process.env.ACTIVE_THEME + '/assets/src/js/cookies.js', 'public/js')
-    .postCss('./themes/' + process.env.ACTIVE_THEME + '/assets/src/css/styles.css', 'public/css')
-    .tailwind()
-    .options({
-        processCssUrls: false
-    })
-    .sourceMaps(false)
     .browserSync({
         proxy: process.env.APP_URL,
         host: process.env.APP_URL,
